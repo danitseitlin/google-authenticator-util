925544d3c1f2838ac4f57920b160a523075ab235
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-08-25 02:09:01 +0300

    .

--- tests/unit-tests.ts
+++ tests/unit-tests.ts
@@ -3,7 +3,7 @@ import { cliArguments } from 'cli-argument-parser';
 import { expect } from 'chai';
 let authenticator: GoogleAuthenticator;
 describe('Tests', async function() {
-    this.timeout(15 * 100 * 60);
+    this.timeout(2 * 100 * 60);
     before(async () => {
         authenticator = new GoogleAuthenticator({
             clientId: cliArguments.clientId,
