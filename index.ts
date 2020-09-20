e4d553d260c46603d7289b6fc6e4fe4b5b5ec86a
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-09-21 01:41:19 +0300

    .

--- index.ts
+++ index.ts
@@ -155,7 +155,8 @@ export class GoogleAuthenticator {
             const headless = (cliArguments.headless === 'false') ? false: true;
             const browser = await puppeteer.launch({
                 executablePath: revisionInfo.executablePath,
-                headless: headless
+                headless: headless,
+                args: ['headless']
             });
             const page = await browser.newPage();
             await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
