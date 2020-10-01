3f7bc9a22f486a1c43a70e2a6f902cf605a81db2
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-10-02 01:27:56 +0300

    .

--- tests/integration.ts
+++ tests/integration.ts
@@ -22,96 +22,69 @@ describe('Tests', async function() {
         }); 
     });
     it('authorizeWithNewToken', async () => {
-        try {
-            await authenticator.authorizeWithNewToken({
-                username: cliArguments.username,
-                password: cliArguments.password,
-                scope: scope
-            })
-            const emails = await filterEmails({
-                auth: authenticator.oAuth2Client,
-                q: emailQuery
-            })
-            expect(emails.length).to.be.greaterThan(0, 'The emails count')
-        } catch (err) {
-            throw new Error(err)
-        }
+        await authenticator.authorizeWithNewToken({
+            username: cliArguments.username,
+            password: cliArguments.password,
+            scope: scope
+        })
+        const emails = await filterEmails({
+            auth: authenticator.oAuth2Client,
+            q: emailQuery
+        })
+        expect(emails.length).to.be.greaterThan(0, 'The emails count')
     });
+
     it('authorizeWithTokenFile', async () => {
-        try {
-            await authenticator.authorizeWithTokenFile({
-                name: `${cliArguments.clientId}-token.json`,
-                directory: './tokens'
-            });
-            const emails = await filterEmails({
-                auth: authenticator.oAuth2Client,
-                q: emailQuery
-            })
-            expect(emails.length).to.be.greaterThan(0, 'The emails count')
-        } catch (err) {
-            throw new Error(err)
-        }
+        await authenticator.authorizeWithTokenFile({
+            name: `${cliArguments.clientId}-token.json`,
+            directory: './tokens'
+        });
+        const emails = await filterEmails({
+            auth: authenticator.oAuth2Client,
+            q: emailQuery
+        })
+        expect(emails.length).to.be.greaterThan(0, 'The emails count')
     });
     it('authorizeWithToken', async () => {
-        try {
-            await authenticator.authorizeWithToken(require(`${process.env.INIT_CWD}/tokens/${cliArguments.clientId}-token.json`))
-            const emails = await filterEmails({
-                auth: authenticator.oAuth2Client,
-                q: emailQuery
-            })
-            expect(emails.length).to.be.greaterThan(0, 'The emails count')
-        } catch (err) {
-            throw new Error(err)
-        }
+        await authenticator.authorizeWithToken(require(`${process.env.INIT_CWD}/tokens/${cliArguments.clientId}-token.json`))
+        const emails = await filterEmails({
+            auth: authenticator.oAuth2Client,
+            q: emailQuery
+        })
+        expect(emails.length).to.be.greaterThan(0, 'The emails count')
     });
     it('waitForEmail', async () => {
-        try {
-            const emails = await waitForEmail({
-                auth: authenticator.oAuth2Client,
-                q: emailQuery
-            })
-            expect(emails.length).to.be.greaterThan(0, 'The emails count')
-        } catch (err) {
-            throw new Error(err)
-        }
+        const emails = await waitForEmail({
+            auth: authenticator.oAuth2Client,
+            q: emailQuery
+        })
+        expect(emails.length).to.be.greaterThan(0, 'The emails count')
     });
-    it('sendEmail & deleteEmail', async () => {
-        try {
-            emailQuery = `from: ${emailFrom} to: ${emailTo} subject: ${emailSubject} is:unread`
-            await sendEmail({to: emailTo, 'from': emailFrom, subject: emailSubject, message: emailMessage, auth: authenticator.oAuth2Client})
-        } catch (err) {
-            throw new Error(err)
-        }
+    it('sendEmail & deleteEmail', async () => {     
+        emailQuery = `from: ${emailFrom} to: ${emailTo} subject: ${emailSubject} is:unread`
+        await sendEmail({to: emailTo, 'from': emailFrom, subject: emailSubject, message: emailMessage, auth: authenticator.oAuth2Client})
     })
     it('getEmail', async () => {
-        try {
-            emails = await waitForEmail({
-                auth: authenticator.oAuth2Client,
-                q: emailQuery
-            })
-            expect(emails.length).to.be.greaterThan(0, 'The emails count')
-            const email = await getEmail({
-                auth: authenticator.oAuth2Client,
-                id: emails[0].id
-            })
-            expect(email.data.raw).contains(emailSubject, 'The title of the email')
-        } catch (err) {
-            throw new Error(err)
-        }
+        emails = await waitForEmail({
+            auth: authenticator.oAuth2Client,
+            q: emailQuery
+        })
+        expect(emails.length).to.be.greaterThan(0, 'The emails count')
+        const email = await getEmail({
+            auth: authenticator.oAuth2Client,
+            id: emails[0].id
+        })
+        expect(email.data.raw).contains(emailSubject, 'The title of the email')
     });
     it('deleteEmail', async () => {
-        try {
-            await deleteEmail({
-                auth: authenticator.oAuth2Client,
-                id: emails[0].threadId
-            });
-            emails = await filterEmails({
-                auth: authenticator.oAuth2Client,
-                q: emailQuery
-            })
-            expect(emails.length).to.be.equal(0, 'The emails count')
-        } catch (err) {
-            throw new Error(err)
-        }
+        await deleteEmail({
+            auth: authenticator.oAuth2Client,
+            id: emails[0].threadId
+        });
+        emails = await filterEmails({
+            auth: authenticator.oAuth2Client,
+            q: emailQuery
+        })
+        expect(emails.length).to.be.equal(0, 'The emails count')
     });
 });
